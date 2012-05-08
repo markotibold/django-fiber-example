689ad485d9d7305de6bf68ff24290db040c2b230
Marko Tibold <marko@tibold.nl>
2012-05-08 13:07:54 -0700

    update example to using Django REST Framework.

--- settings_default.py
+++ settings_default.py
@@ -120,11 +120,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'piston',
     'mptt',
     'compressor',
     'pages',
     'fiber',
+    'djangorestframework'
 )
 
 # A sample logging configuration. The only tangible logging
